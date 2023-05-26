01200eebfef8b3757c409103fe0c6c2891e811da
Clemens Hofmann <clemens.hofmann@student.tugraz.at>
2023-05-26 18:11:19 +0200

    fix zoom bug and continous loading of topojson

--- src/Components/Worldmap/worldmap.js
+++ src/Components/Worldmap/worldmap.js
@@ -1,51 +1,48 @@
-import React, {useEffect, useRef} from 'react'
+import {useEffect, useRef} from 'react'
 import * as d3 from 'd3'
 import './worldmap.css'
-import {useData} from "./useData";
 
-export default function Worldmap() {
-  const geoJson = useData();
+const projection = d3.geoNaturalEarth1();
+const path = d3.geoPath(projection);
+const graticule = d3.geoGraticule();
+const width = 1200;
+const height = 500;
+
+export default function Worldmap({geoJson}) {
   const svgRef = useRef();
   const gref = useRef();
+  const handleZoom = ({transform}) => {
+    gref.current.setAttribute('transform', transform.toString());
+  };
 
-    useEffect(() => {
-        const svgElement = d3.select(svgRef.current);
+  const zoom = d3.zoom()
+    .extent([[0, 0], [width, height]])
+    .scaleExtent([1, 12])
+    .on('zoom', handleZoom);
 
-        const zoom = d3.zoom()
-            .extent([[0, 0], [1200, 500]])
-            .scaleExtent([1, 4])
-            .on('zoom', handleZoom);
-        svgElement.call(zoom);
-
-        return () => {
-            svgElement.on('.zoom', null); // Cleanup zoom event listener
-        };
-    }, []);
-    const handleZoom = ({transform}) => {
-
-      gref.current.setAttribute('transform', transform.toString());
+  useEffect(() => {
+    const svgElement = d3.select(svgRef.current);
+    svgElement.call(zoom);
+    return () => {
+      svgElement.on('.zoom', null);
     };
+  });
 
-    if (!geoJson) {
-        return <pre>Loading...</pre>;
-    }
-
-    const projection = d3.geoNaturalEarth1();
-    const path = d3.geoPath(projection);
-    const graticule = d3.geoGraticule();
-
+  if (!geoJson) {
+    return <pre>Loading...</pre>;
+  }
   return (
-      <div className="Up-Worldmap">
-        <svg ref={svgRef} width={1200} height={500}>
-            <g className="marks" ref={gref}>
-                <path className="sphere" d={path({ type: 'Sphere' })} /* outline of the globe */ />
-                <path className="graticules" d={path(graticule())} />
-                {geoJson.countries.features.map(feature => (
-                    <path className="country" d={path(feature)} />
-                ))}
-                <path className="interiors" d={path(geoJson.interiors)} />
-            </g>
-        </svg>
-      </div>
+    <div className="Up-Worldmap">
+      <svg ref={svgRef} width={width} height={height}>
+        <g className="marks" ref={gref}>
+          <path className="sphere" d={path({type: 'Sphere'})} /* outline of the globe */ />
+          <path className="graticules" d={path(graticule())}/>
+          {geoJson.countries.features.map(feature => (
+            <path className="country" d={path(feature)}/>
+          ))}
+          <path className="interiors" d={path(geoJson.interiors)}/>
+        </g>
+      </svg>
+    </div>
   )
 }
