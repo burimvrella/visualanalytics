47a6f97b8c9c268e98bf7ba8e5a4ff69aee26cc7
Clemens Hofmann <clemens.hofmann@student.tugraz.at>
2023-06-05 16:55:08 +0200

    cleanup treemap implementation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Settings from './Components/Settings/settings.js';
 import Worldmap from './Components/Worldmap/worldmap.js';
 import Treemap from './Components/Treemap/treemap.js';
 import Scatter from './Components/Scatterplot/scatterplot.js';
-import {useData} from "./Components/Worldmap/useData.js";
+import {useData} from './Components/Worldmap/useData.js';
+import  {LoadTreeMapData} from './Components/Treemap/LoadTreeMapData.js';
 
 function App() {
   const [data] = useState([[90, 20], [20, 100], [66, 44], [53, 80], [24, 182], [80, 72], [10, 76], [33, 150], [100, 15]]);
   const geoJson = useData();
+  const treemapData = LoadTreeMapData();
 
   return (
     <div className="App">
       <Settings data={data}/>
       <Worldmap geoJson={geoJson}/>
-      <Treemap/>
+      <Treemap data={treemapData}/>
       <Scatter data={data}/>
     </div>
   );
--- src/Components/Treemap/treemap.js
+++ src/Components/Treemap/treemap.js
@@ -1,127 +1,14 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './treemap.css'
 import * as d3 from 'd3';
 
 const width = 600;
 const height = 400;
-const asdf =  {
-  name: 'Celtics',
-  children: [
-    {
-      name: 'Guards',
-      children: [
-        {
-          category: 'Guards',
-          name: 'Kemba Walker',
-          value: 20.4,
-        },
-        {
-          category: 'Guards',
-          name: 'Marcus Smart',
-          value: 12.9,
-        },
-        {
-          category: 'Guards',
-          name: 'Brad Wanamaker',
-          value: 6.9,
-        },
-        {
-          category: 'Guards',
-          name: 'Tremont Waters',
-          value: 3.6,
-        },
-        {
-          category: 'Guards',
-          name: 'Carsen Edwards',
-          value: 3.3,
-        },
-        {
-          category: 'Guards',
-          name: 'Romeo Langford',
-          value: 2.5,
-        },
-      ],
-    },
-    {
-      name: 'Forwards',
-      children: [
-        {
-          category: 'Forwards',
-          name: 'Jayson Tatum',
-          value: 23.4,
-        },
-        {
-          category: 'Forwards',
-          name: 'Jaylen Brown',
-          value: 20.3,
-        },
-        {
-          category: 'Forwards',
-          name: 'Gordon Hayward',
-          value: 17.5,
-        },
-        {
-          category: 'Forwards',
-          name: 'Grant Williams',
-          value: 3.4,
-        },
-        {
-          category: 'Forwards',
-          name: 'Javonte Green',
-          value: 3.4,
-        },
-        {
-          category: 'Forwards',
-          name: 'Semi Ojeleye',
-          value: 3.4,
-        },
-        {
-          category: 'Forwards',
-          name: 'Vincent Poirier',
-          value: 1.9,
-        },
-      ],
-    },
-    {
-      name: 'Centers',
-      children: [
-        {
-          category: 'Centers',
-          name: 'Daniel Theis',
-          value: 9.2,
-        },
-        {
-          category: 'Centers',
-          name: 'Enes Kanter',
-          value: 8.1,
-        },
-        {
-          category: 'Centers',
-          name: 'Robert Williams III',
-          value: 5.2,
-        },
-        {
-          category: 'Centers',
-          name: 'Tacko Fall',
-          value: 3.3,
-        },
-      ],
-    },
-  ],
-};
 
-export default function Treemap() {
+export default function Treemap({data}) {
   const svgRef = useRef(null);
-  let data= null;
 
-  d3.csv('./data_hierarchy_1level.csv').then(csv_data => {
-    console.log(csv_data)
-    data = csv_data
-    renderTreemap()
-  });
-  console.log(data)
-
-  function renderTreemap() {
+  function RenderTreemap() {
     const svg = d3.select(svgRef.current);
 
     svg.attr('width', width).attr('height', height);
@@ -130,11 +17,10 @@ export default function Treemap() {
       .id(function(d) { return d.name; })   // Name of the entity (column name is name in csv)
       .parentId(function(d) { return d.parent; })   // Name of the parent (column name is parent in csv)
       (data);
-    root.sum(function(d) { return +d.value })   // Compute the numeric value for each entity
+    root.sum(function(d) { return + d.value })   // Compute the numeric value for each entity
 
     const treemapRoot = d3.treemap().size([width, height]).padding(1)(root);
 
-    console.log(treemapRoot.leaves)
     const nodes = svg
       .selectAll('g')
       .data(treemapRoot.leaves())
@@ -149,20 +35,17 @@ export default function Treemap() {
       .attr('width', (d) => d.x1 - d.x0)
       .attr('height', (d) => d.y1 - d.y0)
       .attr('fill', (d) => colorScale(d.data.name));
-
   }
 
-   useEffect(() => {
-     if (data){
-       console.log('There we are!')
-      renderTreemap()
-     }
-   }, [data]);
+  if (!data) {
+    return <pre>Loading...</pre>;
+  }
+  RenderTreemap()
 
   return (
-  <div className="Down-Left-Treemap">
-    <h1>Treemap</h1>
-    <svg ref={svgRef} width={width} height={height}></svg>
-  </div>
+    <div className="Down-Left-Treemap">
+      <h1>Treemap</h1>
+      <svg ref={svgRef} width={width} height={height}></svg>
+    </div>
   )
 }
